a5b5a8e65f63ced91cc9b3237a7e02370f59018f
Sanddep Gupta <sandeepguptavlogs@gmail.com>
2026-01-28 01:44:56 +0530

    datatypes

---
+++ 02_dataTypes.js
@@ -0,0 +1,32 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("hello") // we are using nodejs, not browser
+
+console.log(3 + 3 )
+
+console.log("Sandy");
+
+
+let name = "Sandy"
+let age = 18
+let isLoggedIn = false 
+let state;
+
+
+// number => 2 to power 53 
+// bigint
+// string => ""
+//boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object 
+ 
+
+
