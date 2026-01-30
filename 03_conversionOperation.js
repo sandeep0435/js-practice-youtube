90e5c5b8487646d235001988bab405b7f9a15e45
Sanddep Gupta <sandeepguptavlogs@gmail.com>
2026-01-30 23:54:39 +0530

    Conversion of datatype

---
+++ 03_conversionOperation.js
@@ -0,0 +1,29 @@
+let score = "Sandy"
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log( valueInNumber);
+
+
+//"33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = "Sandy" 
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn);
+
+// 1 => true; o => false
+// "" => false
+// "Sandy" => true
+
+let someNumber = 33 
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log( typeof stringNumber);
+
